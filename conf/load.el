582cddb013d1ad53d20e62ef14900f6ee39fa160
Jacob Scaggs <jacscaggs@gmail.com>
2019-08-12 22:19:19 -0400

    Added some eshell stuff

--- conf/load.el
+++ conf/load.el
@@ -12,3 +12,15 @@
   (use-package dap-node)
   (use-package dap-go)
 )
+
+(use-package esh-autosuggest
+  :hook (eshell-mode . esh-autosuggest-mode)
+  ;; If you have use-package-hook-name-suffix set to nil, uncomment and use the
+  ;; line below instead:
+  ;; :hook (eshell-mode-hook . esh-autosuggest-mode)
+  :ensure t)
+(defun my-eshell-remove-pcomplete ()
+  (remove-hook 'completion-at-point-functions #'pcomplete-completions-at-point t))
+
+;;  ESHELL FIX
+(add-hook 'eshell-mode-hook #'my-eshell-remove-pcomplete)
